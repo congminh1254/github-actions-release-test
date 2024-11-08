bf2dab206fbeb7f7c2725a71bf636c230f426d0a
Artur Jankowski <ajankowski@box.com>
2024-11-08 09:37:40 +0100

    Feat: Update Tests.swift

--- Example/Tests/Tests.swift
+++ Example/Tests/Tests.swift
@@ -8,6 +8,7 @@ class Tests: XCTestCase {
         // Put setup code here. This method is called before the invocation of each test method in the class.
     }
     
+    
     override func tearDown() {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
         super.tearDown()
